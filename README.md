305ad273e97cb9af9bfc3fa9160d3069a8095c37
onlymisaky <onlymisaky@gmail.com>
2022-03-03 14:13:18 +0800

    ✨ feat: 完成开发

---
+++ README.md
@@ -0,0 +1,36 @@
+# vite-plugin-define-process-env
+
+## 介绍
+
+Vue-cli 项目迁移到 vite 时，需要将原有的环境变量前缀从 `VUE_APP_` 修改为 `VITE_` ，同时将业务代码中的 `process.env.VUE_APP_XXX` 修改为 `import.meta.env.VITE_XXX` 。如果你不想手动完成这一工作，可以尝试使用本插件，不需要修改任何业务代码即可轻松完成环境变量的迁移和使用。
+
+同时，如果你不喜欢 `vite` 将环境变量定义到 `import.meta.env` 这一方式，你也可以使用本插件来代替 `define` 这一配置项。
+
+## 安装
+
+```bash
+npm i vite-plugin-define-process-env -D
+
+yarn add vite-plugin-define-process-env -D
+```
+
+## 使用
+```ts
+// vite.config.ts
+import { defineConfig } from "vite";
+import vitePluginDefineProcessEnv from "vite-plugin-define-process-env";
+
+export default defineConfig({
+  ...
+  plugins: [vitePluginDefineProcessEnv()]
+});
+```
+
+## 配置项
+
+```ts
+interface Options {
+  path: string; // .env 文件所在的目录，默认为跟目录
+  key: string; // 要定义的环境变量名称，默认为 process.env
+}
+```
---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import * as path from 'path';
+import * as fs from 'fs';
+import { Plugin } from 'vite';
+import * as dotenv from 'dotenv';
+
+interface Options {
+  path: string;
+  key: string;
+}
+
+export default function vitePluginDefineProcessEnv(
+  option: Partial<Options> = { path: '', key: 'process.env' },
+): Plugin {
+  return {
+    name: 'vite-plugin-define-process-env',
+    config(config, { command, mode }) {
+      const filepath = path.resolve(__dirname, option.path as string);
+      let files: string[] = [];
+      if (fs.existsSync(filepath)) {
+        files = fs.readdirSync(filepath);
+      }
+      const envs = files
+        .filter((filename, index, arr) => [
+          '.env',
+          '.env.local',
+          `.env.${mode}`,
+          `.env.${mode}.local`
+        ].includes(filename))
+        .sort((a, b) => a.length - b.length)
+        .reduce((prev, filename, index, arr) => ({
+          ...prev,
+          ...dotenv.config({
+            path: path.resolve(filepath, filename),
+          }).parsed,
+        }), {});
+
+      return {
+        define: {
+          [option.key as string]: envs,
+        },
+      };
+    },
+  };
+}
